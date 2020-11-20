f1da92df031949c30bccf028860b31ce5d337db3
jishnu <jishnuek8055@gmail.com>
2020-11-20 15:50:13 +0530

    view for completed items

--- todolist/views.py
+++ todolist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Todolist
 from .forms import TodoListForm
 from django.views.decorators.http import require_POST
@@ -17,4 +17,10 @@ def addTodoItem(request):
     if form.is_valid():
         new_todo = Todolist(text=request.POST['text'])
         new_todo.save()
+    return redirect('index')
+
+def completedTodo(request, todo_id):
+    todo = Todolist.objects.get(pk=todo_id)
+    todo.completed = True
+    todo.save()
     return redirect('index')
