849f15754a5957cfb64f5387d8bfdd31e783127c
jishnu <jishnuek8055@gmail.com>
2020-11-20 13:46:32 +0530

    adding form input to database

--- todolist/forms.py
+++ todolist/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
-class TodoListForm(for,s.Form):
-    text = forms.CharField(max-length=45,
+class TodoListForm(forms.Form):
+    text = forms.CharField(max_length=45,
         widget = forms.TextInput(
             attrs = {'class' : 'form-control', 'placeholder' : 'Enter todo e.g. Wash my car', 'aria-label' : 'Todo', 'aria-describedby' : 'dd-btn'}))
     
--- todolist/views.py
+++ todolist/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Todolist
+from .forms import TodoListForm
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def index(request):
     todo_items = Todolist.objects.order_by('id')
-    context = {'todo_items': todo_items}
+    form = TodoListForm()
+    context = {'todo_items': todo_items, 'form' : form}
     return render(request, 'todolist/index.html', context)
+
+@require_POST
+def addTodoItem(request):
+    form = TodoListForm(request.POST)
+    # print(request.POST['text'])
+    if form.is_valid():
+        new_todo = Todolist(text=request.POST['text'])
+        new_todo.save()
+    return redirect('index')
